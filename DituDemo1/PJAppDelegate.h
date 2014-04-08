bf725a35ebdd0a3b62dab010632233a736c7573c
hamigua <hamigua@hamiguadeiMac.local>
2014-04-08 10:28:33 +0800

    Initial Commit

---
+++ DituDemo1/PJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PJAppDelegate.h
+//  DituDemo1
+//
+//  Created by hamigua on 14-4-8.
+//  Copyright (c) 2014年 com.patk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DituDemo1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DituDemo1
+//
+//  Created by hamigua on 14-4-8.
+//  Copyright (c) 2014年 com.patk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PJAppDelegate class]));
+    }
+}
